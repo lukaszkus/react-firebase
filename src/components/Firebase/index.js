5741febe153ca534d781cb1edabf8051d3b634b6
lukaszkus <kusiu@vp.pl>
2021-02-10 13:27:46 +0100

    firebase connected

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,11 +1,6 @@
-import React from 'react'
-
-function Firebase() {
-    return (
-        <div>
-            <h1>Firebase</h1>
-        </div>
-    )
-}
-
-export default Firebase
+import FirebaseContext from './context';
+import Firebase from './firebase';
+ 
+export default Firebase;
+ 
+export { FirebaseContext };
